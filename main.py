e8df4fec4fee9f36a50e76c8623d5e9298a2ddf8
daniel <dhrc1997@gmail.com>
2020-11-27 01:31:17 -0300

    Several changes

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
                     # retorna todos resultados - proximo if filtra
                     if num in array_group:
                         # print(aluno.cod_aluno, aluno.cod_area, i, j, num)
-                        alunos_relations.append([aluno.cod_aluno, aluno.cod_area, j])
+                        alunos_relations.append([aluno.cod_aluno, aluno.cod_area, j, num])
                         # print(alunos_relations)
             if alunos_relations != []:
                 grupos.append(alunos_relations)
@@ -65,87 +65,12 @@ if __name__ == '__main__':
     # grupo[0] = cod_aluno
     # grupo[1] = cod_area
     # grupo[2] = cod_outra_area
+    # grupo[3] = dissimilaridade
+
+
     for gru in grupos:
+        print(gru)
         for mini_gru in gru:
-            print(mini_gru)
+            pass
+           # print(mini_gru)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                # if aluno.cod_area == i and aluno.cod_area != j:
-                #     # pegar o conteudo num de line.
-                #
-                #     if num > simi:
-                #         print('aluno:', aluno.cod_aluno, ' cod_area: ', j,' similaridade: ', num)
-
-                       # print(aluno.cod_aluno,' similaridade: ', num)
-
-                    # if aluno.cod_area > simi:
-                    #     for alun2 in alunos:
-                    #         if alun2.cod_aluno != aluno.cod_aluno:
-                    #             if alun2.cod_area == j:
-
-                                    # arquivo2[i][j]
-
-
-
-
-
-
-
-
-
-    # navegar matriz
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #         similar_array.append(num)
-    # print(" similar: ", similar_array)
-    #
-    # # selecionando a similaridade baseada em k grupos
-    # for num in similar_array:
-    #     if num !=0 and num not in similar_aux_dict.keys():
-    #         similar_aux_dict[num] = similar_array.count(num)/2
-    #
-    # print(similar_aux_dict)
-
-
-    # Percorrer similaridade e pegar k grup
-
-
-
-    # aluno_similar_to = {}
-    #
-    # for aluno in alunos:
-    #     for i, line in enumerate(arquivo2):
-    #         for j, column in enumerate(line):
-    #             aluno_similar_to[aluno.cod_area] = grupo.detecta_area_similar(aluno.cod_area, j, column, similar_aux_dict.values())
-    #
